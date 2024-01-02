e06080c9c482217e0c5b738410becfa9031cf18c
Luc Thien <hsr.rework@gmail.com>
2024-01-02 23:44:07 +0700

    nghíia da di cau ca

--- Models/Cart.php
+++ Models/Cart.php
@@ -9,46 +9,55 @@ class Cart extends Model
     {
         $this->db = new Database();
     }
-    public function getCart() {
+    public function getCart()
+    {
 
         $select = 'SELECT * FROM cart INNER JOIN sanpham ON cart.id_product = sanpham.id_san_pham';
         $result = $this->db->cart($select);
         return $result;
     }
-    public function sl() {
+    public function sl()
+    {
         $select = 'SELECT id FROM cart';
         $result = $this->db->cart($select);
         $count = count($result);
         return $count;
     }
 
-    public function addCart($id, $name, $price, $quantity, $id_us) {
+    public function addCart($id, $name, $price, $quantity, $id_us)
+    {
 
         $sql = "INSERT INTO cart (id_product, name_product, price, quantity, id_us) VALUES ('$id','$name','$price','$quantity','$id_us')";
         $insert = $this->db->signUp($sql);
         return $insert;
     }
-    public function rm($id) {
-        $select = 'DELETE FROM cart WHERE id ='.$id;
+    public function rm($id)
+    {
+        $select = 'DELETE FROM cart WHERE id =' . $id;
         $result = $this->db->cart($select);
         return $result;
     }
-    public function countItem() {
+    public function countItem()
+    {
         $select = "SELECT SUM(quantity) FROM cart";
         $result = $this->db->cart($select);
         return $result;
     }
-    public function sale() {
+    public function sale()
+    {
         $select = "SELECT SUM(price) FROM cart";
         $result = $this->db->cart($select);
         return $result;
     }
-    public function insertInvoice($id_cart, $id_user, $payment) {
+    public function insertInvoice($id_cart, $id_user, $payment)
+    {
         $sql = "INSERT INTO hoa_don (id_cart, id_user, payment) VALUES ('$id_cart','$id_user','$payment')";
         $insert = $this->db->Invoice($sql);
         return $insert;
     }
-    public function clearCart() {
-        trs
+    public function clearCart()
+    {
+        // trs
+        // nghia da ia ơ day
     }
 }
